29aca3f7ef0ca9de04ea75e1ba222ec33b6e3620
Devam Trivedi <102553689+Devamstark@users.noreply.github.com>
2025-08-02 20:37:02 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,25 @@
-touch scripts/deploy.js scripts/monitor.js
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  
+  console.log("Deploying with account:", deployer.address);
+  
+  const HoneypotFactory = await ethers.getContractFactory("HoneypotFactory");
+  const factory = await HoneypotFactory.deploy();
+  
+  await factory.deployed();
+  console.log("Factory deployed to:", factory.address);
+  
+  // Deploy sample honeypots
+  const tx1 = await factory.createHoneypot(0, { value: ethers.utils.parseEther("0.1") });
+  const receipt1 = await tx1.wait();
+  console.log("Basic honeypot:", receipt1.events[0].args.contractAddress);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
