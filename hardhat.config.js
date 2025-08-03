b3198351f776ae2572f9e8566227de31c8fa6f08
Devam Trivedi <102553689+Devamstark@users.noreply.github.com>
2025-08-02 20:39:10 -0400

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
