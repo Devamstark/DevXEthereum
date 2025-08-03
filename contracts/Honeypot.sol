f7714d677d9e967e49b09ce47598df581c3ac008
Devam Trivedi <102553689+Devamstark@users.noreply.github.com>
2025-08-02 20:36:45 -0400

    Update Honeypot.sol

--- contracts/Honeypot.sol
+++ contracts/Honeypot.sol
@@ -1,1 +1,29 @@
-touch contracts/Honeypot.sol contracts/HoneypotFactory.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./BasicHoneypot.sol";
+import "./ReentrancyHoneypot.sol";
+import "./HiddenOwnerHoneypot.sol";
+
+contract HoneypotFactory {
+    enum HoneypotType { Basic, Reentrancy, HiddenOwner }
+    
+    event HoneypotCreated(address indexed contractAddress, HoneypotType hType);
+    
+    function createHoneypot(HoneypotType _type) external payable returns (address) {
+        address newContract;
+        
+        if (_type == HoneypotType.Basic) {
+            newContract = address(new BasicHoneypot{value: msg.value}());
+        } 
+        else if (_type == HoneypotType.Reentrancy) {
+            newContract = address(new ReentrancyHoneypot{value: msg.value}());
+        }
+        else if (_type == HoneypotType.HiddenOwner) {
+            newContract = address(new HiddenOwnerHoneypot{value: msg.value}());
+        }
+        
+        emit HoneypotCreated(newContract, _type);
+        return newContract;
+    }
+}
